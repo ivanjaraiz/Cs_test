a3c7283768894f33a1f3c498deb27cd6db16e340
IJM <ivan.jaraiz@gmail.com>
2020-05-29 03:21:30 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cs_test
+{
+    class Program
+    {
+       public  enum Colors { Red, Green, Blue, Yellow }
+        static void Main(string[] args)
+        {
+            object[] rangeModel = Enum.GetNames(typeof(Colors));
+            Console.WriteLine(rangeModel[0]);
+            Console.WriteLine(rangeModel[1]);
+            Console.WriteLine(rangeModel[2]);
+            Console.WriteLine(rangeModel[3]);
+
+        }
+    }
+}
